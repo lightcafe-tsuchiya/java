a5537785cdb91ace098df235bcd62ca6217792ea
toratora <torafusu@gmail.com>
2020-02-10 00:11:18 +0900

    DB環境追加

---
+++ MySQL/docker/stop.sh
@@ -0,0 +1,2 @@
+# docker exec -it コンテナ名 mysql -uユーザ名 -pパスワード -D DB名
+docker-compose stop
---
+++ MySQL/env/initdb.d/testdata.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id,name,age) VALUES (1, 'TOM','20');
+INSERT INTO users (id,name,age) VALUES (2, 'JERRY','30');
